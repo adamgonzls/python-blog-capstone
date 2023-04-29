186dfaffcc552896c46466381242b6bc3e0fdeff
Adam Gonzales <adamgonzales1@gmail.com>
2023-04-28 18:28:32 -0600

    feature: add partial for footer, update static resources to use dynamic url

--- server.py
+++ server.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template, request
 import requests
 from blog import Blog
+import smtplib
+import os
 
 app = Flask(__name__)
 BLOG_API = "https://api.npoint.io/b58e4135e9c76d950b95"
+SMTP_ADDRESS = "smtp.gmail.com"
+SENDING_EMAIL = "adamgonzalestest@gmail.com"
+SEND_TO_EMAIL = "adamgonzales1@gmail.com"
+GMAIL_APP_PASSWORD = os.environ.get("GMAIL_APP_PASSWORD")
+MAIL_SUBMISSION_PORT = 587
 
 res = requests.get(url=BLOG_API).json()
 print(res)
@@ -26,23 +33,13 @@ def about():
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
     if request.method == "POST":
-        name = request.form['name']
-        phone = request.form['phone']
-        email = request.form['email']
-        message = request.form['message']
-        print(name, phone, email, message)
+        requestor_name = request.form['name']
+        requestor_phone = request.form['phone']
+        requestor_email = request.form['email']
+        requestor_message = request.form['message']
+        send_email(requestor_name, requestor_phone, requestor_email, requestor_message)
         return render_template("contact.html", message_sent=True)
     return render_template("contact.html", message_sent=False)
-
-
-# @app.route("/form-entry", methods=["POST"])
-# def receive_data():
-#     name = request.form['name']
-#     phone = request.form['phone']
-#     email = request.form['email']
-#     message = request.form['message']
-#     print(name, phone, email, message)
-#     return render_template("contact.html", confirmation="Successfully sent message")
 
 
 @app.route("/post/<int:id>")
@@ -50,6 +47,18 @@ def get_post(id):
     for blog in blog_objects:
         if blog.id == id:
             return render_template("post-details.html", blog_object=blog)
+
+
+def send_email(requestor_name, requestor_phone, requestor_email, requestor_message):
+    email_message = f"subject: Contact form submitted by: {requestor_name}\n\nName: {requestor_name}\nEmail: {requestor_email}\nPhone: {requestor_phone}\nMessage: {requestor_message}\n"
+    with smtplib.SMTP(SMTP_ADDRESS, MAIL_SUBMISSION_PORT) as connection:
+        connection.starttls()
+        connection.login(user=SENDING_EMAIL, password=GMAIL_APP_PASSWORD)
+        connection.sendmail(
+            from_addr=SENDING_EMAIL,
+            to_addrs=SEND_TO_EMAIL,
+            msg=email_message
+        )
 
 
 if __name__ == "__main__":
